76c3500e98e9360cd470b83db68ed568bcabc72f
Lyndon Jonhson <jonhsoncabral13@gmail.com>
2022-08-18 10:52:43 -0300

    Adding study about Generics, Set and Map

---
+++ src/genericsAndSetAndMap/generics2/application/Program.java
@@ -0,0 +1,40 @@
+package genericsAndSetAndMap.generics2.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import genericsAndSetAndMap.generics2.entities.Product;
+import genericsAndSetAndMap.generics2.services.CalculationService;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        String path = "C:\\Users\\jonhs\\IdeaProjects\\java-study\\src\\generics\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+
+            Product x = CalculationService.max(list);
+            System.out.println("Most expensive:");
+            System.out.println(x);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
